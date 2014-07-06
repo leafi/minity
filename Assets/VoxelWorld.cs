e8b118af8a499195fc651a82428387db54e01dbd
leafi <leaf@leaf.sx>
2014-07-06 21:53:26 +0100

    progress commit; 3D textures, unity pro only, but it works!

---
+++ Assets/VoxelWorld.cs
@@ -0,0 +1,196 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class VoxelWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    //gameObject.AddComponent("MeshFilter");
+    //gameObject.AddComponent("MeshRenderer");
+    Mesh mesh = GetComponent<MeshFilter>().mesh;
+    /*mesh.Clear();
+    mesh.vertices = new Vector3[] {new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Vector3(1, 1, 0)};
+    mesh.uv = new Vector2[] {new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 1)};
+    var g = new Color32(0, 255, 0, 255);
+    mesh.colors32 = new Color32[] { new Color32(255, 0, 0, 255), g, new Color32(0, 0, 255, 255) };
+    mesh.triangles = new int[] {0, 1, 2};*/
+
+    mesh.Clear();
+    List<Vector3> meshVertices = new List<Vector3>();
+    List<Vector2> meshUv = new List<Vector2>();
+    List<int> meshTriangles = new List<int>();
+
+    const bool backface = true;
+    //const float the_diff = -0.99f / 256.0f;
+    const float the_diff = 0f;
+
+    // X planes
+    for (int x = 0; x < 256; x++) {
+      int v_i = meshVertices.Count;
+      float ecks = -0.5f + (x / 256.0f);
+      meshVertices.Add(new Vector3(ecks, -0.5f, 0.5f));
+      meshUv.Add(new Vector2(0.0f, 0.0f));
+      meshVertices.Add(new Vector3(ecks, 0.5f, 0.5f));
+      meshUv.Add(new Vector2(0.0f, 1.0f));
+      meshVertices.Add(new Vector3(ecks, 0.5f, -0.5f));
+      meshUv.Add(new Vector2(1.0f, 1.0f));
+      meshVertices.Add(new Vector3(ecks, -0.5f, -0.5f));
+      meshUv.Add(new Vector2(1.0f, 0.0f));
+
+      meshTriangles.AddRange(new int[] { v_i + 2, v_i + 1, v_i, v_i, v_i + 3, v_i + 2 });
+
+
+      if (backface) {
+        v_i += 4;
+        ecks -= the_diff;
+        meshVertices.Add(new Vector3(ecks, -0.5f, 0.5f));
+        meshUv.Add(new Vector2(0.0f, 0.0f));
+        meshVertices.Add(new Vector3(ecks, 0.5f, 0.5f));
+        meshUv.Add(new Vector2(0.0f, 1.0f));
+        meshVertices.Add(new Vector3(ecks, 0.5f, -0.5f));
+        meshUv.Add(new Vector2(1.0f, 1.0f));
+        meshVertices.Add(new Vector3(ecks, -0.5f, -0.5f));
+        meshUv.Add(new Vector2(1.0f, 0.0f));
+        meshTriangles.AddRange(new int[] { v_i, v_i + 1, v_i + 2, v_i + 2, v_i + 3, v_i });
+
+        // inside
+        //meshTriangles.AddRange(new int[] { v_i + 2, v_i + 1, v_i, v_i, v_i + 3, v_i + 2 });
+      }
+    }
+
+    // Y planes
+    for (int y = 0; y < 256; y++) {
+      int v_i = meshVertices.Count;
+      float ecks = -0.5f + (y / 256.0f);
+      meshVertices.Add(new Vector3(-0.5f, ecks, -0.5f));
+      meshUv.Add(new Vector2(0.0f, 0.0f));
+      meshVertices.Add(new Vector3(-0.5f, ecks, 0.5f));
+      meshUv.Add(new Vector2(0.0f, 1.0f));
+      meshVertices.Add(new Vector3(0.5f, ecks, 0.5f));
+      meshUv.Add(new Vector2(1.0f, 1.0f));
+      meshVertices.Add(new Vector3(0.5f, ecks, -0.5f));
+      meshUv.Add(new Vector2(1.0f, 0.0f));
+
+      meshTriangles.AddRange(new int[] { v_i, v_i + 1, v_i + 2, v_i + 2, v_i + 3, v_i });
+
+      if (backface) {
+        v_i += 4;
+        ecks -= the_diff;
+        meshVertices.Add(new Vector3(-0.5f, ecks, -0.5f));
+        meshUv.Add(new Vector2(0.0f, 0.0f));
+        meshVertices.Add(new Vector3(-0.5f, ecks, 0.5f));
+        meshUv.Add(new Vector2(0.0f, 1.0f));
+        meshVertices.Add(new Vector3(0.5f, ecks, 0.5f));
+        meshUv.Add(new Vector2(1.0f, 1.0f));
+        meshVertices.Add(new Vector3(0.5f, ecks, -0.5f));
+        meshUv.Add(new Vector2(1.0f, 0.0f));
+        //meshTriangles.AddRange(new int[] { v_i, v_i + 1, v_i + 2, v_i + 2, v_i + 3, v_i });
+        
+        // inside
+        meshTriangles.AddRange(new int[] { v_i + 2, v_i + 1, v_i, v_i, v_i + 3, v_i + 2 });
+      }
+    }
+
+    // Z planes
+    for (int y = 0; y < 256; y++) {
+      int v_i = meshVertices.Count;
+      float ecks = -0.5f + (y / 256.0f);
+      meshVertices.Add(new Vector3(-0.5f, -0.5f, ecks));
+      meshUv.Add(new Vector2(0.0f, 0.0f));
+      meshVertices.Add(new Vector3(-0.5f, 0.5f, ecks));
+      meshUv.Add(new Vector2(0.0f, 1.0f));
+      meshVertices.Add(new Vector3(0.5f, 0.5f, ecks));
+      meshUv.Add(new Vector2(1.0f, 1.0f));
+      meshVertices.Add(new Vector3(0.5f, -0.5f, ecks));
+      meshUv.Add(new Vector2(1.0f, 0.0f));
+
+      meshTriangles.AddRange(new int[] { v_i + 2, v_i + 1, v_i, v_i, v_i + 3, v_i + 2 });
+
+      if (backface) {
+        v_i += 4;
+        ecks -= the_diff;
+        meshVertices.Add(new Vector3(-0.5f, -0.5f, ecks));
+        meshUv.Add(new Vector2(0.0f, 0.0f));
+        meshVertices.Add(new Vector3(-0.5f, 0.5f, ecks));
+        meshUv.Add(new Vector2(0.0f, 1.0f));
+        meshVertices.Add(new Vector3(0.5f, 0.5f, ecks));
+        meshUv.Add(new Vector2(1.0f, 1.0f));
+        meshVertices.Add(new Vector3(0.5f, -0.5f, ecks));
+        meshUv.Add(new Vector2(1.0f, 0.0f));
+        meshTriangles.AddRange(new int[] { v_i, v_i + 1, v_i + 2, v_i + 2, v_i + 3, v_i });
+        
+        // inside
+        //meshTriangles.AddRange(new int[] { v_i + 2, v_i + 1, v_i, v_i, v_i + 3, v_i + 2 });
+      }
+    }
+
+    mesh.vertices = meshVertices.ToArray();
+    mesh.uv = meshUv.ToArray();
+    mesh.triangles = meshTriangles.ToArray();
+
+
+
+
+    var colors = new List<Color32>();
+    for (int i = 0; i < mesh.vertices.Length; i++) {
+      var v = mesh.vertices[i];
+      var vv = new Vector3(v.x, v.y, v.z);
+
+      if (backface && (i % 8 > 3)) {
+        vv.x += 1.0f / 256.0f;
+        vv.y += 1.0f / 256.0f;
+        vv.z += 1.0f / 256.0f;
+      }
+
+      // each vertex coord must be in range [-0.5,0.5]
+      colors.Add(new Color32((byte)(255 * (vv.x + 0.5)),
+        (byte)(255 * (vv.y + 0.5)),
+        (byte)(255 * (vv.z + 0.5)),
+        (byte)254));
+    }
+    mesh.colors32 = colors.ToArray();
+
+    mesh.RecalculateBounds();
+    mesh.RecalculateNormals();
+
+
+    //const int VOXEL_TEX_LENGTH = 2048;
+    const int WORLD_LENGTH = 256;
+
+
+    // test voxel texture
+    /* TextureFormat.Alpha8 ? */
+    /* or one of the numerous 16-bit formats... */
+    //#if (!UNITY_PRO_LICENSE)
+    //throw new Exception("3D textures don't work in free Unity. You need to 
+    var voxels = new Texture3D(WORLD_LENGTH, WORLD_LENGTH, WORLD_LENGTH, TextureFormat.RGBA32, false);
+    voxels.filterMode = FilterMode.Point;
+    var vox = new Color[WORLD_LENGTH * WORLD_LENGTH * WORLD_LENGTH];
+    /*for (int i = 0; i < vox.Length; i++) {
+      //vox[i] = new Color32(15 * 16 + 1, 15 * 16 + 0, 14 * 16, 14 * 16 + 1);
+      vox[i] = new Color32(1, 1, 1, 1);
+    }*/
+
+    for (int z = 0; z < WORLD_LENGTH; z++) {
+      for (int y = 0; y < WORLD_LENGTH; y++) {
+        for (int x = 0; x < WORLD_LENGTH; x++) {
+          vox[z * WORLD_LENGTH * WORLD_LENGTH + (y * WORLD_LENGTH) + x] = (
+             Random.value > 0.9f ? new Color(y / 256.0f, y / 256.0f, y / 256.0f, y / 256.0f) : new Color(0f, 0f, 0f, 0f));
+        }
+      }
+    }
+    voxels.SetPixels(vox);
+    voxels.Apply();
+
+    renderer.material.SetTexture("_Voxels", voxels);
+
+
+    //GetComponent<MeshRenderer>().material.SetTexture("UEOAOU", lookupTex);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    //renderer.transform.Rotate(0f, 20f * Time.deltaTime, 0f);
+	}
+}
---
+++ Assets/VoxelWorldShader.shader
@@ -0,0 +1,67 @@
+﻿Shader "Custom/VoxelWorldShader" {
+	Properties {
+		_TextureAtlas ("TextureAtlas (RGB/RGBA)", 2D) = "white" {}
+		_Voxels ("Voxels (RGBA) (Dynamic)", 3D) = "white" {}
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		//LOD 200
+		//Blend SrcAlpha OneMinusSrcAlpha 
+		//Cull Off
+		
+		CGPROGRAM
+		#pragma surface surf Lambert
+		//#pragma surface surf Flat
+		//#pragma profileoption MaxTexIndirections=5
+		#pragma glsl
+		#pragma target 3.0
+		  
+        half4 LightingFlat (SurfaceOutput s, half3 lightDir, half atten) {
+            return half4(s.Albedo.r, s.Albedo.g, s.Albedo.b, 1.0) * 0.8;
+        }
+         
+		sampler2D _TextureAtlas;
+		sampler3D _Voxels;
+
+		struct Input {
+			//float3 worldPos;
+			float2 uv_TextureAtlas;
+			float3 color : COLOR;
+			float3 worldNormal;
+		};
+
+		void surf (Input IN, inout SurfaceOutput o) {
+
+			float WORLD_LENGTH = 256.0;
+			float VOXEL_TEX_LENGTH = 2048.0;
+
+			float GRID_SIZE = 16.0;
+			
+			float3 coords = floor(WORLD_LENGTH * IN.color.rgb);
+
+			float4 vl2 = tex3D(_Voxels, (coords) / WORLD_LENGTH);
+			float answer = WORLD_LENGTH * vl2.a;
+			
+			if (answer < 1.0) {
+				discard;
+				o.Albedo = half3(0.0, 0.0, 0.0);
+				o.Alpha = 0.0;
+			}
+			
+			float2 uv0 = float2(floor(fmod(answer, GRID_SIZE)), floor(answer / GRID_SIZE)) / GRID_SIZE + 
+		 		16.0 * fmod(IN.uv_TextureAtlas, 1.0/256.0);
+
+			float2 uv_dx = ddx( IN.uv_TextureAtlas );
+			float2 uv_dy = ddy( IN.uv_TextureAtlas );
+
+			float4 rgbb = tex2D(_TextureAtlas, uv0, uv_dx, uv_dy);
+
+			//float4 rgbb = tex2D(_TextureAtlas, uv0);
+
+			o.Albedo = rgbb.rgb;
+			o.Alpha = rgbb.a;
+		}
+
+		ENDCG
+	} 
+}
